103db71cfeb5cf1e94e906531e6209edf9d05cc6
tianyl1984 <tianyl1984@qq.com>
2016-06-17 06:59:37 +0800

    add bean properties

--- src/main/java/com/tianyl/Application.java
+++ src/main/java/com/tianyl/Application.java
@@ -1,9 +1,20 @@
 package com.tianyl;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.tianyl.bean.FooBean;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		FooBean foo = context.getBean(FooBean.class);
+		System.out.println(foo.getName());
 
+		context.close();
 	}
 
 }
